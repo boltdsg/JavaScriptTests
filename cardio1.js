8800dba079591a5b120d7629eccdfa880fd3c2b9
thellecodes <60751900+thellecodes@users.noreply.github.com>
2022-07-02 09:18:13 -0700

    cardio sessions

---
+++ cardio1.js
@@ -0,0 +1,61 @@
+//Reverse string
+function reverseString(str) {
+  //   return str.split("").reverse().join("");
+
+  ////////////////////////////////////
+  let revString = "";
+  //   for (let i = str.length - 1; i >= 0; i--) {
+  //     revString = revString + str[i];
+  //   }
+  // return revString;
+
+  ////////////////////////////////////
+  //   for (let i = 0; i <= str.length - 1; i++) {
+  //     revString = str[i] + revString;
+  //   }
+
+  ////////////////////////////////////
+  //   for (let char of str) {
+  //     revString = str[char] + revString;
+  //   }
+
+  ////////////////////////////////////
+  //   str.split("").forEach((char) => {
+  //     revString = char + revString;
+  //   });
+
+  ////////////////////////////////////
+  //   return str.split("").reduce((revString, char) => char + revString, "");
+}
+// console.log(reverseString("samuel"));
+
+function isPalindrome(str) {
+  //   return str.split(" ").reverse().join("") === str;
+}
+// console.log(isPalindrome("racecar"))
+
+function reverseInt(int) {
+  //   const revString = int.toString().split("").reverse().join("");
+  //   return parseInt(revString) * Math.sign(int);
+}
+// console.log(reverseInt(123));
+
+function capitalizeLetters(str) {
+  //   const strArr = str.toLowerCase().split(" ");
+  //   for (let i = 0; i < strArr.length; i++) {
+  //     strArr[i] =
+  //       strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
+  //   }
+  //   return strArr.join(" ");
+  ////////////////////////////////////
+  //   return str
+  //     .toLowerCase()
+  //     .split(" ")
+  //     .map((word) => word[0].toUpperCase() + word.substr(1))
+  //     .join(" ");
+}
+// console.log(capitalizeLetters("i love javascript"));
+
+function maxCharacters(str) {}
+
+console.log(maxCharacters("samuel is coming home right to this place now"));
